3d9b3043b2d958665ef499375c6e5b2ecce7fc82
ShaikLalbi <42571266+ShaikLalbi@users.noreply.github.com>
2018-08-27 20:32:38 +0530

    Update Consonant.py

--- Consonant.py
+++ Consonant.py
@@ -1,6 +1,13 @@
-d=['a','e','i','o','u','A','E','I','O','U']
-s=raw_input()
-if(s in d):
+i=raw_input()
+if(i=='a'):
+	print("Vowel")
+elif(i=='e'):
+	print("Vowel")
+elif(i=='o'):
+	print("Vowel")
+elif(i=='i'):
+	print("Vowel")
+elif(i=='u'):
 	print("Vowel")
 else:
 	print("Consonant")
