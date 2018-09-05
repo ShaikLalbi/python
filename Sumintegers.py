df72b5398130542378cee9b4d141812598b92555
ShaikLalbi <42571266+ShaikLalbi@users.noreply.github.com>
2018-09-05 10:02:50 +0530

    Update Sumintegers.py

--- Sumintegers.py
+++ Sumintegers.py
@@ -1,7 +1,7 @@
-N,K=raw_input().split( )
+N,K=map(int,raw_input().split( ))
 N=list(map(int,raw_input().split()))
 sum=0
-for i in range(0,int(K)):
-	sum=sum+int(K)
+for i in range(0,(K+1)):
+	sum=sum+i
 	N.append(sum)
 print(sum)
